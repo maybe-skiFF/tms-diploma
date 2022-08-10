c4b99e4b084a83b01316ba107709a895a7b5d483
maybe-skiFF <den.skiff190@gmail.com>
2022-08-10 20:39:29 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import { Provider } from "./context/Context";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Books from "./components/Books";
+import BookCart from "./components/BookCart";
+import BookDetails from "./components/BookDetails";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <Fragment>
+                <Header />
+                <Books />
+              </Fragment>
+            )}
+          />
+          <Route path="/book-cart" component={BookCart} />
+          <Route path="/book/details/:id" component={BookDetails} />
+        </Switch>
+      </div>
+    </Provider>
   );
 }
 
